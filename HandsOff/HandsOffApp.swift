a31732277cef16314d23cdf6be16519d9b92262f
Tristan <tristanchay123@gmail.com>
2023-11-24 22:44:01 +0800

    Initial Commit

---
+++ HandsOff/HandsOffApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandsOffApp.swift
+//  HandsOff
+//
+//  Created by Tristan Chay on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HandsOffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
